f241ed55d7c2d1e9c3d0bc972aa086fee7be0048
chaebinn <juchaebin975@gmail.com>
2026-01-23 18:02:43 +0900

    feat: 스플래시 화면 추가

--- app/src/main/java/com/example/quickeng/MainActivity.kt
+++ app/src/main/java/com/example/quickeng/MainActivity.kt
@@ -21,32 +21,9 @@ class MainActivity : ComponentActivity() {
         setContent {
             QuickEngTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                    QuickEngTheme {
-                        MainScaffold()
-                    }
+                    MainScaffold(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
-    }
-}
-
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    QuickEngTheme {
-        Greeting("Android")
     }
 }
